e963a9c6d6c4ab9f6eeb7d276fd1455f8e44f0e3
Alejandro Celaya <alejandrocelaya@gmail.com>
2024-06-20 10:48:57 +0200

    Migrate to ESLint flat config

--- index.js
+++ index.js
@@ -1,79 +1,95 @@
-module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended'
-  ],
-  plugins: ['jsx-a11y', 'simple-import-sort', '@stylistic'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
-  rules: {
-    '@stylistic/arrow-parens': 'error',
-    '@stylistic/arrow-spacing': 'error',
-    '@stylistic/block-spacing': 'error',
-    '@stylistic/comma-dangle': ['error', 'always-multiline'],
-    '@stylistic/eol-last': 'error',
-    '@stylistic/function-call-spacing': 'error',
-    '@stylistic/indent': ['error', 2],
-    '@stylistic/key-spacing': 'error',
-    '@stylistic/keyword-spacing': 'error',
-    '@stylistic/max-len': [
-      'error',
-      // Do not allow more than 120 characters per line, except for long strings and comments in the same line
-      { 'code':  120, 'ignoreComments': true, 'ignoreStrings': true, 'ignoreTemplateLiterals': true },
+import eslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import react from 'eslint-plugin-react';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
     ],
-    '@stylistic/no-trailing-spaces': 'error',
-    '@stylistic/object-curly-spacing': ['error', 'always'],
-    '@stylistic/quotes': ['error', 'single'],
-    '@stylistic/jsx-quotes': ['error', 'prefer-double'],
-    '@stylistic/rest-spread-spacing': 'error',
-    '@stylistic/semi': 'error',
-    '@stylistic/spaced-comment': 'error',
-    '@stylistic/no-multiple-empty-lines': ['error', { 'max': 1 }],
-
-    '@typescript-eslint/consistent-type-imports': 'error',
-
-    'simple-import-sort/imports': ['error', {
-      'groups': [
-        // First external imports, then local imports, then styles imports
-        ['^', '^\\.', '\\.s?css$']
-      ]
-    }],
-    'no-restricted-exports': ['error', {
-      'restrictDefaultExports': {
-        'direct': true,
-        'named': true,
-        'defaultFrom': true,
-        'namedFrom': true,
-        'namespaceFrom': true
+    plugins: {
+      '@stylistic': stylistic,
+      'simple-import-sort': simpleImportSort,
+      'jsx-a11y': pluginJsxA11y,
+      react,
+      'react-hooks': eslintPluginReactHooks, // TODO Check if eslint/compat is needed here
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect'
       }
-    }],
+    },
+    rules: {
+      ...pluginJsxA11y.configs.recommended.rules,
+      ...eslintPluginReactHooks.configs.recommended.rules,
 
-    // Disabled rules from presets
-    'react/display-name': ['off', { 'ignoreTranspilerName': false }],
-    'react/prop-types': 'off',
-    '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
+      '@stylistic/arrow-parens': 'error',
+      '@stylistic/arrow-spacing': 'error',
+      '@stylistic/block-spacing': 'error',
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/eol-last': 'error',
+      '@stylistic/function-call-spacing': 'error',
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/key-spacing': 'error',
+      '@stylistic/keyword-spacing': 'error',
+      '@stylistic/max-len': [
+        'error',
+        // Do not allow more than 120 characters per line, except for long strings and comments in the same line
+        { 'code':  120, 'ignoreComments': true, 'ignoreStrings': true, 'ignoreTemplateLiterals': true },
+      ],
+      '@stylistic/no-trailing-spaces': 'error',
+      '@stylistic/object-curly-spacing': ['error', 'always'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/jsx-quotes': ['error', 'prefer-double'],
+      '@stylistic/rest-spread-spacing': 'error',
+      '@stylistic/semi': 'error',
+      '@stylistic/spaced-comment': 'error',
+      '@stylistic/no-multiple-empty-lines': ['error', { 'max': 1 }],
+
+      '@typescript-eslint/consistent-type-imports': 'error',
+
+      'simple-import-sort/imports': ['error', {
+        'groups': [
+          // First external imports, then local imports, then styles imports
+          ['^', '^\\.', '\\.s?css$']
+        ]
+      }],
+      'no-restricted-exports': ['error', {
+        'restrictDefaultExports': {
+          'direct': true,
+          'named': true,
+          'defaultFrom': true,
+          'namedFrom': true,
+          'namespaceFrom': true
+        }
+      }],
+
+      // Disabled rules from presets
+      'react/display-name': ['off', { 'ignoreTranspilerName': false }],
+      'react/prop-types': 'off',
+      '@typescript-eslint/ban-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
   },
-  overrides: [
-    {
-      files: ['*.test.*', '*.spec.*'],
-      rules: {
-        'prefer-promise-reject-errors': 'off',
-        'no-param-reassign': 'off',
-        'react/no-children-prop': 'off',
-        '@typescript-eslint/no-shadow': 'off'
-      }
+  {
+    files: ['*.test.*', '*.spec.*'],
+    rules: {
+      'prefer-promise-reject-errors': 'off',
+      'no-param-reassign': 'off',
+      'react/no-children-prop': 'off',
+      '@typescript-eslint/no-shadow': 'off'
     }
-  ]
-};
+  },
+);
